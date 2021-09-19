6d69417abf4564d9c7800cfe9f632527a258966a
SASWATI HALDER <90272181+haldersas@users.noreply.github.com>
2021-09-19 12:37:04 -0600

    Updated documentation and args

--- nth_power.py
+++ nth_power.py
@@ -1,8 +1,11 @@
-def nth_power(n, power):
+def nth_power(n, power=2):
 	"""
 	calculates power for numbers upto n
+	args:
+	     n: highest number in the list of numbers
+	     power: power for numbers to raise, default power is 2
 	"""
 
 	return [i**power for i in range(n)]
 
-print(nth_power(10,4))
+print(nth_power(10))
